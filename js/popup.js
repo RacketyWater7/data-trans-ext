42078f57afb5dacb902a2d879c39be43a747522d
RacketyWater7 <haseebudeen@outlook.com>
2022-07-25 13:31:07 +0500

    added pupup.js and popup.html

--- js/popup.js
+++ js/popup.js
@@ -1,200 +1,183 @@
-fetchOptions();
-let website = document.getElementById("website");
-if (website) {
-  website.addEventListener("click", function () {
-    chrome.tabs.create({ url: "https://lakport.utl.gov.in/Home.aspx" });
-  });
-}
+const options = (message) => {
+  return {
+    type: "basic",
+    title: "Data Trans",
+    message: message,
+    iconUrl: "/images/48.png",
+  };
+};
 
-let add_passenger = document.getElementById("add_passenger");
-if (add_passenger) {
-  add_passenger.addEventListener("click", function (e) {
+let copy_address = document.getElementById("copy_address");
+if (copy_address) {
+  copy_address.addEventListener("click", function (e) {
     e.preventDefault();
-    let table = document.getElementById("passenger_table");
-    let row = table.insertRow(table.rows.length);
-    let cell1 = row.insertCell(0);
-    let cell2 = row.insertCell(1);
-    let cell3 = row.insertCell(2);
-    let cell4 = row.insertCell(3);
-    let cell5 = row.insertCell(4);
-    // add input fields to the cells
-    cell1.innerHTML = `<input type="text" class="form-control" id="name" >`;
-    cell2.innerHTML = `<input type="number" class="form-control" min="0" id="photo_id">`;
-    cell3.innerHTML = `<input type="number" class="form-control" min="0" id="age" >`;
-    cell4.innerHTML = `<select name="gender" class="form-control input-field" id="gender">
-                         <option value="0">Male</option>
-                         <option value="1">Female</option>
-                       </select>`;
-    cell5.innerHTML = `<select name="category" id="category" class="form-control input-field">
-                        <option value="Select Category" >
-                          Select Category
-                        </option>
-                        <option value="G?1">Government Employee</option>
-                        <option value="I?1" selected="selected">Islander</option>
-                        <option value="P?2">Permit</option>
-                        <option value="R?1">Relatives</option>
-                        <option value="Z?1">Senior Citizen</option>
-                      </select>`;
-  });
-}
-let save = document.getElementById("save");
-if (save) {
-  save.addEventListener("click", function (e) {
-    e.preventDefault();
-    let ship = document.getElementById("ship").value;
-    let source = document.getElementById("source").value;
-    let destination = document.getElementById("destination").value;
-    let classs = document.getElementById("class").value;
-    let passengers = document.getElementById("passengers").value;
-    let infants = document.getElementById("infants").value;
-    let mobile = document.getElementById("mobile").value;
-    const fixedDetails = {
-      ship: ship,
-      source: source,
-      destination: destination,
-      class: classs,
-      passengers: passengers,
-      infants: infants,
-      mobile: mobile,
-    };
-    const passengerDetails = [];
+    let { hostname } = window.location;
+    switch (hostname) {
+      case "seller.walmart.com": {
+        (async function () {
+          await sleep(1000);
+          try {
+            console.log("copying wallmart address");
+            let addressClass = document.getElementsByClassName("G42Nv")[1];
+            let fullName = addressClass.firstChild.innerText;
+            let fullAddress =
+              addressClass.firstChild.nextElementSibling.innerText;
+            fullAddress = fullAddress.split(",");
+            // 221 Fairlamb Ave, Havertown, PA, 19083, USA
+            let address = fullAddress[0];
+            let city = fullAddress[1];
+            let state = fullAddress[2];
+            let zip = fullAddress[3];
 
-    // passengerDetails.push({ id: Date.now(), passengerName, photoID, age });
-    let table = document.getElementById("passenger_table");
-    // save all the details of the passengers
-    for (let i = 1; i < table.rows.length; i++) {
-      let name = table.rows[i].cells[0].children[0].value;
-      let photoID = table.rows[i].cells[1].children[0].value;
-      let age = table.rows[i].cells[2].children[0].value;
-      let gender = table.rows[i].cells[3].children[0].value;
-      let category = table.rows[i].cells[4].children[0].value;
-      passengerDetails.push({
-        id: i,
-        name: name,
-        photoID: photoID,
-        age: age,
-        gender: gender,
-        category: category,
-      });
-    }
+            const wallmart_address = {
+              fullName,
+              address,
+              city,
+              state,
+              zip,
+            };
+            chrome.storage.sync.set(
+              {
+                wallmart_address: wallmart_address,
+              },
+              function () {
+                console.log("Wallmart address is saved");
+                chrome.notifications.create(
+                  options("Wallmart Address saved successfully")
+                );
+              }
+            );
+          } catch (error) {
+            console.log("error while saving walmart address: ", error);
+          }
+        })();
 
-    chrome.storage.sync.set(
-      {
-        fixedDetails: fixedDetails,
-        passengerDetails: passengerDetails,
-      },
-      function () {
-        console.log("Data is saved: ");
+        break;
       }
-    );
-    chrome.storage.sync.get(
-      ["fixedDetails", "passengerDetails"],
-      function (result) {
-        if (result && result.fixedDetails) {
-          console.log(
-            "Fixed Details: ",
-            result.fixedDetails,
-            "Passenger Details: ",
-            result.passengerDetails
-          );
-        }
-      }
-    );
-    let options = {
-      type: "basic",
-      title: "Lakport",
-      message: "Data saved successfully",
-      iconUrl: "/images/48.png",
-    };
-    chrome.notifications.create(options);
-  });
-}
+      case "sellercentral.amazon.com": {
+        (async function () {
+          try {
+            // John Jensen
+            // 6603 N IL ROUTE 2
+            // OREGON, IL 61061-9327
+            console.log("copying amazon address");
+            sleep(1000);
+            let fullAddress =
+              document.getElementsByClassName("a-normal")[2].innerText;
+            fullAddress = fullAddress.split(`\n`);
+            let fullName = fullAddress[0];
+            let address = fullAddress[1];
+            let address2Bundle = fullAddress[2];
+            let zip = address2Bundle.split(/[^0-9]/);
+            zip = `${zip[zip.length - 2]}`;
+            let state = address2Bundle.split(/[^a-zA-Z]/);
+            console.log("preState:", state);
+            state = state[2] === "" ? state[3] : state[2];
+            address2Bundle = address2Bundle.split(",");
+            let city = address2Bundle[0];
+            // let state = address2Bundle[1].split(" ")[0];
+            // let zip = address2Bundle[1].split(" ")[1];
 
-let clear = document.getElementById("clear");
-if (clear) {
-  clear.addEventListener("click", function (e) {
-    e.preventDefault();
-    // clearing all the fields
-    document.getElementById("ship").value = "Select Ship";
-    document.getElementById("source").value = "Select Source";
-    document.getElementById("destination").value = "Select Destination";
-    document.getElementById("class").value = "Select Class";
-    document.getElementById("passengers").value = "";
-    document.getElementById("infants").value = "";
-    document.getElementById("mobile").value = "";
-    // clearing the table
-    let table = document.getElementById("passenger_table");
-    if (table.rows.length > 2) {
-      for (let i = table.rows.length - 1; i > 1; i--) {
-        table.deleteRow(i);
+            // console.log("fullName", fullName);
+            // console.log("address", address);
+            // console.log("city", city);
+            // console.log("state", state);
+            // console.log("zip", zip);
+            const amazon_address = {
+              fullName,
+              address,
+              city,
+              state,
+              zip,
+            };
+            chrome.storage.sync.set(
+              {
+                amazon_address: amazon_address,
+              },
+              function () {
+                console.log("Amazon address is saved");
+                chrome.notifications.create(
+                  options("Amazon Address saved successfully")
+                );
+              }
+            );
+          } catch (error) {
+            console.log("error while saving amazon address: ", error);
+          }
+        })();
+        break;
       }
-    }
-    // emptying the first row of table; i.e. below the header row
-    table.rows[1].cells[0].children[0].value = "";
-    table.rows[1].cells[1].children[0].value = "";
-    table.rows[1].cells[2].children[0].value = "";
-    table.rows[1].cells[3].children[0].value = "0";
-    table.rows[1].cells[4].children[0].value = "I?1";
-    // clearing the storage
-    chrome.storage.sync.remove(["fixedDetails", "passengerDetails"]);
-  });
-}
+      case "www.pestcontrolwholesale.com": {
+        (async function () {
+          console.log("at pestcontrol");
+          sleep(1000);
 
-/**
- * Function to fetch the existing data from the storage
- */
-function fetchOptions() {
-  chrome.storage.sync.get(
-    ["fixedDetails", "passengerDetails"],
-    function (items) {
-      if (items.fixedDetails) {
-        document.getElementById("ship").value =
-          items.fixedDetails.ship === null
-            ? "Select Ship"
-            : items.fixedDetails.ship;
-        document.getElementById("source").value =
-          items.fixedDetails.source === null
-            ? "Select Source"
-            : items.fixedDetails.source;
-        document.getElementById("destination").value =
-          items.fixedDetails.destination;
-        document.getElementById("class").value = items.fixedDetails.class;
-        document.getElementById("passengers").value =
-          items.fixedDetails.passengers;
-        document.getElementById("infants").value = items.fixedDetails.infants;
-        document.getElementById("mobile").value = items.fixedDetails.mobile;
+          let fullAddress =
+            document.getElementsByClassName("address")[1].innerText;
+
+          fullAddress = fullAddress ? fullAddress.split(`\n`) : [];
+          let fullName, address2, address, addressBundle;
+          if (fullAddress.length === 4) {
+            fullName = fullAddress[0];
+            address2 = fullAddress[1];
+            address = fullAddress[2];
+            addressBundle = fullAddress[3];
+          } else {
+            fullName = fullAddress[0];
+            address = fullAddress[1];
+            addressBundle = fullAddress[2];
+          }
+          try {
+            let city = addressBundle.split(",")[0];
+
+            let state = addressBundle.split(",")[1].split(" ")[1];
+            let zip = addressBundle.split(",")[1].split(" ")[2];
+            // console.log("fullName", fullName);
+            // console.log("address", address);
+            // console.log("address2", address2);
+            // console.log("city", city);
+            // console.log("state", state);
+            // console.log("zip", zip);
+            let woocommerce_address;
+            if (address2) {
+              woocommerce_address = {
+                fullName,
+                address,
+                address2,
+                city,
+                state,
+                zip,
+              };
+            } else {
+              woocommerce_address = {
+                fullName,
+                address,
+                city,
+                state,
+                zip,
+              };
+            }
+
+            chrome.storage.sync.set(
+              {
+                woocommerce_address: woocommerce_address,
+              },
+              function () {
+                console.log("Woocommerce address is saved");
+                chrome.notifications.create(
+                  options("Woocommerce Address saved successfully")
+                );
+              }
+            );
+          } catch (error) {
+            console.log("error in saving pestControl address", error);
+          }
+        })();
+        break;
       }
-      if (items.passengerDetails) {
-        let table = document.getElementById("passenger_table");
-        for (let i = 0; i < items.passengerDetails.length; i++) {
-          let row = table.insertRow(table.rows.length - 1);
-          let cell1 = row.insertCell(0);
-          let cell2 = row.insertCell(1);
-          let cell3 = row.insertCell(2);
-          let cell4 = row.insertCell(3);
-          let cell5 = row.insertCell(4);
-          // add input fields to the cells
-          cell1.innerHTML = `<input type="text" class="form-control" id="name" value="${items.passengerDetails[i].name}">`;
-          cell2.innerHTML = `<input type="number" class="form-control" min="0" id="photo_id" value="${items.passengerDetails[i].photoID}">`;
-          cell3.innerHTML = `<input type="number" class="form-control" min="0" id="age" value="${items.passengerDetails[i].age}">`;
-          cell4.innerHTML = `<select name="gender" class="form-control input-field" id="gender">
-                              <option value="0">Male</option>
-                              <option value="1">Female</option>
-                            </select>`;
-          cell5.innerHTML = `<select name="category" id="category" class="form-control input-field">
-                              <option value="Select Category" >
-                                Select Category
-                              </option>
-                              <option value="G?1">Government Employee</option>
-                              <option value="I?1" selected="selected">Islander</option>
-                              <option value="P?2">Permit</option>
-                              <option value="R?1">Relatives</option>
-                              <option value="Z?1">Senior Citizen</option>
-                            </select>`;
-          cell4.children[0].value = items.passengerDetails[i].gender;
-          cell5.children[0].value = items.passengerDetails[i].category;
-        }
+      default: {
+        chrome.notifications.create(options("Invalid website address"));
       }
     }
-  );
+  });
 }
